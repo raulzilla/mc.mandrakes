73309bb816b9d7a6bdd14c3ac9c232eca6c7ed3d
raulzilla <raulzilla.dev@gmail.com>
2021-10-31 16:05:52 -0300

    ajuste

--- clipboard.js
+++ clipboard.js
@@ -1,5 +1,7 @@
 function clipboard() {
   let copyText = document.getElementById("input-copy");
+  let copyIp = document.getElementById("copy-ip");
+
 
   copyText.select();
   copyText.setSelectionRange(0, 99999);
